13aaeaab6c347f05e167b5b69e5ca20c3fd69a79
Diogo L Scarmagnani <diogols@live.com>
2023-05-04 10:41:08 -0300

    botoes

--- src/Botoes.js
+++ src/Botoes.js
@@ -1,17 +1,23 @@
 function Botoes({incrementar, decrementar}) {
     return (
         <div className="flex  w-1/2 h-24">
-            <div className="flex justify-center items-center  w-1/2 h-full">
+            <div className="flex justify-center items-center w-1/2 h-full">
                 <button
-                    className="bg-green-500 hover:bg-green-700 text-white  font-bold h-16 w-16 rounded-full flex items-center justify-center"
+                    className={`
+                        bg-green-500 hover:bg-green-700 text-white font-bold
+                        h-16 w-16 rounded-full flex items-center justify-center
+                    `}
                     onClick={incrementar}
                 >
                     +
                 </button>
             </div>
-            <div className="flex justify-center items-center  w-1/2 h-full">
+            <div className="flex justify-center items-center w-1/2 h-full">
                 <button
-                    class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold h-12 w-12 rounded-full"
+                    className={`
+                        bg-yellow-500 hover:bg-yellow-700 text-white font-bold
+                        h-12 w-12 rounded-full
+                    `}
                     onClick={decrementar}
                 >
                     -
